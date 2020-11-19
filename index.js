c0364b35bd49153b4674bf3d430989873cbabb9e
J <jonathan.arnould33@gmail.com>
2020-11-19 16:57:28 +0100

    fix connexion

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const connection = require("./config");
 const express = require("express");
 const port = process.env.PORT || 3000;
 const app = express();
 const routes = require("./routes");
 
+const cors = require("cors");
+
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
