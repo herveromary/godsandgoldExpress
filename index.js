ccdc750d82bb364e2fc3996f771f3489dcffa00d
fdureux <fanny.dureux@gmail.com>
2020-11-19 15:44:54 +0100

    changed port const

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const connection = require("./config");
 const express = require("express");
-const port = 3000;
+const port = process.env.PORT || 3000;
 const app = express();
 const routes = require("./routes");
 
