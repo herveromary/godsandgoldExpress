81fdbb6924acaf0f57db99e00772c6ccce99a4b6
surlejonathan <surle.jonathan@gmail.com>
2020-12-26 21:52:40 +0100

    pool instead of connection in config.js

--- config.js
+++ config.js
@@ -3,11 +3,12 @@ const mysql = require("mysql");
 
 dotenv.config();
 
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
   host: process.env.MYSQL_HOST,
   database: process.env.MYSQL_DATABASE,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
+  debug: true,
 });
 
-module.exports = connection;
+module.exports = pool;
